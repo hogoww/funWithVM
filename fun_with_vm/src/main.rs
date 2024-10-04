d36607998ee5a234ffa8b9f63f22f67a47c6da73
hogoww <pierre_misse25@msn.com>
2024-10-04 10:42:54 +0200

    [fix] split into different files

--- fun_with_vm/src/main.rs
+++ fun_with_vm/src/main.rs
@@ -1,3 +1,6 @@
+pub mod memory_space;
+use crate::memory_space::MemorySpace;
+
 fn main() {
 	let memory_space_size:usize = 1000000;
 	let space = MemorySpace::for_bit_size(memory_space_size);
---
+++ fun_with_vm/src/memory_space.rs
@@ -0,0 +1,19 @@
+pub struct MemorySpace {
+	memory_vector: Vec<usize>
+}
+
+impl MemorySpace {
+
+	pub fn for_bit_size(memory_space_size: usize) -> Self{
+		let mut memory_space: Vec<usize> = Vec::with_capacity(memory_space_size);
+		for _i in 0..memory_space_size {
+			memory_space.push(0);
+		}
+		//TODO set the first OOP as free
+		Self { memory_vector: memory_space }
+	}
+
+	pub fn report(&self){
+		println!("memory_vector = {}" , self.memory_vector.len());
+	}
+}
