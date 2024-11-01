3e92f8fe06c908ae7e897d5c1f066244ff193a58
hogoww <pierre_misse25@msn.com>
2024-11-01 16:19:49 +0100

    [improvement] allocator now relies on the iterator

--- fun_with_vm/src/allocator.rs
+++ fun_with_vm/src/allocator.rs
@@ -1,22 +1,17 @@
 use crate::memory_space::MemorySpace;
-use crate::oop::Oop;
 
 pub fn where_to_allocate(number_of_usize: usize, space: &mut MemorySpace) -> usize {
-    let mut index: usize = space.get_start_index();
-    let last_index = space.get_end_index();
-
-    while index < last_index {
-        let oop: Oop = space.get_oop_at(index);
+    let mut iter = space.iter();
+    while let Some(oop) = iter.next(space) {
         if oop.is_free_oop() && oop.number_of_slots() >= number_of_usize {
             // We found a free index with enough space !
-            return index;
+            return oop.get_index();
         }
-        index = oop.next_oop_index();
     }
     //We didn't find a proper place in memory to put that many usize
 
-    // should probably say we need a GC.
-    // Maybe thrown an eror in the meantime.
+    // Should probably say we need a GC.
+    // Thrown an eror in the meantime.
     std::process::exit(1);
 }
 
